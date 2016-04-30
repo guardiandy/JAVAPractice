1f5dcaf1e4d91d1f0a48f4c3222a7e6a95defb20
guardiandy <kmy.ttlc@gmail.com>
2016-04-30 16:14:52 +0800

    add DemoThis Jave

--- src/practice/DemoThis.java
+++ src/practice/DemoThis.java
@@ -1,5 +1,24 @@
 package practice;
 
 public class DemoThis {
-
+	
+	private String name;
+	private int age;
+	DemoThis(String name, int age){
+		setName(name);
+		setAge(age);
+		this.print();
+	}
+public void setName(String name){
+	this.name=name;
+}
+public void setAge(int age){
+	this.age=age;
+}
+public void print(){
+	System.out.println("Name="+name+"Age="+age);
+}
+public static void main(String[] args){
+	DemoThis dt = new DemoThis("Kevin","22");
+}
 }
