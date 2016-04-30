21dd5bf8bff062a4aa5686ca468fea2c7e1c598f
guardiandy <kmy.ttlc@gmail.com>
2016-04-30 15:57:10 +0800

    First commit

---
+++ src/practice/A.java
@@ -0,0 +1,16 @@
+package practice;
+
+class A 
+{
+int no = 1;
+}
+class Main
+{
+	public static void main(String[] args)
+	{
+		A obj=new A();
+		System.out.println(obj.no);
+
+	}
+
+}
---
+++ src/practice/A2.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class A2 {
+int no = 1;
+public void displayNo()
+{
+	System.out.println(no);
+}
+}
+class Mai
+{
+	public static void main(String[]args)
+	{
+		A obj=new A();
+		System.out.println(obj.no);
+		A ob2=new A();
+		System.out.println(ob2.no);
+	}
+}
